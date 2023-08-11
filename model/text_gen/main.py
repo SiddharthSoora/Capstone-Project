64a170b47f0aa615bf2e788abd3bc800fd980ec0
ExpectoX1 <79239242+ExpectoX1@users.noreply.github.com>
2023-08-11 16:50:21 +0530

    init-dir

---
+++ model/text_gen/main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+st.write("Hello World!")
+
+# import a PDF file
+
+
+# Extract text from pdf 
+def extract_text():
+    pass
+
+# Extract images from pdf
+def extract_images():
+    pass
+
+#pass the corpus to the model 
+
+def model_work_image():
+    return "This is the output of the model"
+
+def model_work_text():
+    return "This is the output of the model"
+
+# Display the output of the model
+def display_output():
+    pass
+
+
