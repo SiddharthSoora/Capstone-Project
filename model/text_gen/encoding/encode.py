0a76c336f3dfb687a34fa3423a534deb984c933f
SiddharthSoora <77402814+SiddharthSoora@users.noreply.github.com>
2023-08-17 12:44:42 +0530

    boolqgen init

---
+++ model/text_gen/encoding/encode.py
@@ -0,0 +1,13 @@
+
+def beam_search_decoding (inp_ids,attn_mask,model,tokenizer):
+  beam_output = model.generate(input_ids=inp_ids,
+                                 attention_mask=attn_mask,
+                                 max_length=256,
+                               num_beams=10,
+                               num_return_sequences=3,
+                               no_repeat_ngram_size=2,
+                               early_stopping=True
+                               )
+  Questions = [tokenizer.decode(out, skip_special_tokens=True, clean_up_tokenization_spaces=True) for out in
+               beam_output]
+  return [Question.strip().capitalize() for Question in Questions]
