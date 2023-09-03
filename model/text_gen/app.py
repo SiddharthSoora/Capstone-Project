2aee887c8e8c91aba2d2348e2ab80a76a03ac8cf
ExpectoX1 <79239242+ExpectoX1@users.noreply.github.com>
2023-09-04 01:05:37 +0530

    app.py

--- model/text_gen/app.py
+++ model/text_gen/app.py
@@ -2,21 +2,42 @@ from pprint import pprint
 import nltk
 import streamlit as st
 import sys
-nltk.download('stopwords')
-sys.path.append('../text_gen/')
-from main import QGen
-qg = QGen()
-payload = {"input_text":'''Law of Segregation
-This law is based on the fact that the alleles do not show any blending
-and that both the characters are recovered as such in the F2
- generation
-though one of these is not seen at the F1
- stage. Though the parents contain
-two alleles during gamete formation, the factors or alleles of a pair segregate
-from each other such that a gamete receives only one of the two factors.
-Of course, a homozygous parent produces all gametes that are similar
-while a heterozygous one produces two kinds of gametes each having
-one allele with equal proportion.'''}
-output = qg.predict_mcq(payload)
-pprint (output)
-st.write(output)
+
+
+
+st.set_page_config(layout="wide")
+
+def generate_questions(payload):
+    nltk.download('stopwords')
+    sys.path.append('../text_gen/')
+    from main import QGen
+    question_generation = QGen()
+    if(payload == "'input_text':"):
+        output = "Please enter some text to generate questions"
+    output = question_generation.predict_mcq(payload)
+    return output
+
+def Streamlit():
+    out = ""
+    col1,col2 = st.columns(2)
+    with col1:
+        st.header('Input Text')
+        input_text = st.text_area('Input Text',height=500)
+        payload = {"input_text": input_text}
+        out = st.button('Generate Questions')
+        print(out)
+    with col2:
+        if out  == True:
+            out = generate_questions(payload)
+            st.header('Output Text')
+            st.text_area('Output Text',value=out,height=500)
+        st.write('')
+
+
+if __name__ == '__main__':
+    Streamlit()
+
+
+
+
+#button - >click -> true or false => click -> true , false
